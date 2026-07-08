b6d0ecb39c94d019fc290edbe332f982edaef8db
isdore95 <echefula4@gmail.com>
2026-07-08 11:21:03 +0100

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,22 +1,20 @@
-
+import pandas as pd
 
 def calculate_signal(data):
-
     if data.empty:
         return "WAIT", 0
 
-    close = data["Close"]
+    close = data["Close"].squeeze()
 
     ema20 = close.ewm(span=20).mean()
     ema50 = close.ewm(span=50).mean()
 
-    current20 = ema20.iloc[-1]
-    current50 = ema50.iloc[-1]
+    current20 = float(ema20.iloc[-1])
+    current50 = float(ema50.iloc[-1])
 
     if current20 > current50:
         return "BUY", 85
-
-    if current20 < current50:
+    elif current20 < current50:
         return "SELL", 85
-
-    return "WAIT", 50
+    else:
+        return "WAIT", 50
