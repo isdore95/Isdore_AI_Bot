899ec5db2a74226da987bc90004f217d200f668e
isdore95 <echefula4@gmail.com>
2026-07-12 02:02:35 +0100

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
 def calculate_signal(data):
+
     if data.empty:
         return "WAIT", 0
 
     close = data["Close"].squeeze()
+    high = data["High"].squeeze()
+    low = data["Low"].squeeze()
 
     ema20 = close.ewm(span=20).mean()
     ema50 = close.ewm(span=50).mean()
@@ -16,26 +19,44 @@ def calculate_signal(data):
     rsi = 100 - (100 / (1 + rs))
 
     macd = close.ewm(span=12).mean() - close.ewm(span=26).mean()
-    signal = macd.ewm(span=9).mean()
+    macd_signal = macd.ewm(span=9).mean()
 
-    current20 = float(ema20.iloc[-1])
-    current50 = float(ema50.iloc[-1])
-    current_rsi = float(rsi.iloc[-1])
-    current_macd = float(macd.iloc[-1])
-    current_signal = float(signal.iloc[-1])
+    resistance = high.tail(20).max()
+    support = low.tail(20).min()
 
-    if (
-        current20 > current50
-        and current_rsi < 70
-        and current_macd > current_signal
-    ):
-        return "BUY", 95
+    price = float(close.iloc[-1])
 
-    if (
-        current20 < current50
-        and current_rsi > 30
-        and current_macd < current_signal
-    ):
-        return "SELL", 95
+    score_buy = 0
+    score_sell = 0
+
+    if ema20.iloc[-1] > ema50.iloc[-1]:
+        score_buy += 1
+    else:
+        score_sell += 1
+
+    if macd.iloc[-1] > macd_signal.iloc[-1]:
+        score_buy += 1
+    else:
+        score_sell += 1
+
+    if 45 < rsi.iloc[-1] < 70:
+        score_buy += 1
+
+    if 30 < rsi.iloc[-1] < 55:
+        score_sell += 1
+
+    if price > support:
+        score_buy += 1
+
+    if price < resistance:
+        score_sell += 1
+
+    confidence = max(score_buy, score_sell) * 25
+
+    if score_buy >= 3:
+        return "BUY", confidence
+
+    if score_sell >= 3:
+        return "SELL", confidence
 
     return "WAIT", 60
