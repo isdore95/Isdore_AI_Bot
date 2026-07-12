c07e8cf7ff7fac7e886230bb9f04b3c3393f5d85
isdore95 <echefula4@gmail.com>
2026-07-12 03:24:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from market import get_market_data
 from strategy import calculate_signal
 from signals import send_signal
+from trend import trend_filter
 from risk import calculate_lot, expiry_time
 
 print("=" * 40)
@@ -11,18 +12,29 @@ data = get_market_data()
 
 if data.empty:
     print("No market data received.")
+
 else:
+    trend = trend_filter(data)
+
     signal, confidence = calculate_signal(data)
+
+    if trend == "UP" and signal == "SELL":
+        signal = "WAIT"
+
+    elif trend == "DOWN" and signal == "BUY":
+        signal = "WAIT"
 
     lot = calculate_lot(confidence)
     expiry = expiry_time(confidence)
 
+    print(f"Trend      : {trend}")
     print(f"Signal     : {signal}")
     print(f"Confidence : {confidence}%")
     print(f"Lot Size   : {lot}")
     print(f"Expiry     : {expiry}")
-if signal != "WAIT":
-    send_signal(signal, confidence)
-    print("Telegram signal sent.")
-else:
-    print("No trade setup found.")
+
+    if signal != "WAIT":
+        send_signal(signal, confidence)
+        print("Telegram signal sent.")
+    else:
+        print("No trade setup found.")
